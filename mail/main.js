020dca96e8c01e4f0cfa8197e34a779ed9bef06b
Carlo-Falanga <carlofalanga7@gmail.com>
2026-02-05 13:22:57 +0100

    add array list

--- mail/main.js
+++ mail/main.js
@@ -1,1 +1,21 @@
 console.log('It works');
+
+/*
+Mail
+Crea una lista di email di invitati ad una festa.
+Chiedi all’utente la sua email,
+controlla che sia nella lista di chi può accedere,
+stampa un messaggio appropriato sull’esito del controllo, utilizzando un ciclo for.
+Non è consentito usare nessun metodo proprio degli array (come includes, per esempio).
+Si può fare? Certo che si basta ragionare un po’.
+Nota: 
+Non è necessario provvedere alla validazione delle email
+*/
+
+// - creo una lista di con un array
+const array = ['esempio1@gmail.com', 'esempio2@alice.it', 'esempio3@libero.it']
+console.log(array);
+
+// - chiedo all'utente di inserire la mail
+// - controllo SE la mail inserita e' presente nell'array con un ciclo FOR
+// - stampo messaggio appropriato
